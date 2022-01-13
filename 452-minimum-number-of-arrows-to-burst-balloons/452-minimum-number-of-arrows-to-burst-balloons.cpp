cd18ae1b7babdebf03df00636f3a56efce81191f
Naman Arora <46819389+naman3112@users.noreply.github.com>
2022-01-13 23:58:05 +0530

    Time: 1260 ms (5.09%), Space: 102.4 MB (9.10%) - LeetHub

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if (points.empty()) return 0;
+        sort(points.begin(), points.end());
+        int start = points[0][0], end = points[0][1];
+		
+		// the reason res = 1 is that we need an arrow to destroy the last group. 
+        int res = 1;
+        for (auto point: points)
+        {
+            if (point[0] <= end)
+            {
+                start = max(start, point[0]);
+                end = min(end, point[1]);
+            }
+            else
+            {
+                res++;
+                start = point[0];
+                end = point[1];
+            }
+        }
+        return res;
+    }
+    
+    
+};
